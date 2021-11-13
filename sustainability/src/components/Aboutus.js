385ccdff62f1fee80308054c4d5fe8b2739e7fe3
Tiana Aldroubi <tianama02@gmail.com>
2021-11-13 18:49:08 -0500

    aboutus

---
+++ sustainability/src/components/Aboutus.js
@@ -0,0 +1,81 @@
+import './Aboutus.css'
+import React from "react";
+
+
+
+function Aboutus() {
+  return (
+        <div className="about">
+          <div class="container">
+            <div class="row align-items-center my-5">
+
+              <div class="col-lg-5">
+                <h1 class="font-weight-light">About Us!</h1>
+                <p>
+                  We are lehigh students...
+                </p>
+              </div>
+              <div class="col-lg-7">
+
+                <div class="Ayon">
+                  
+                  <img
+                    class="img-fluid rounded mb-4 mb-lg-0"
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                    alt=""
+                    height = "300"
+                  />
+                  <h2>Ayon Bhowmick</h2>
+                    <p class="paragraph">
+                     dkjafhksjdfh lkdjf l;kasjdf ;lkasjf;lkas jfl;as f;las jf;lka sjf;lka sjdfl;k jf;lka sjdf;lk asj;dfl jasj;l 
+                    </p>
+                </div>
+        
+                <div class="Josh">
+                  <img
+                    class="img-fluid rounded mb-4 mb-lg-0"
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                    alt=""
+                    height ="300"
+                  />
+                  <h2>Josh Tripoli</h2>
+                  <p class="paragraph">
+                     dkjafhksjdfh lkdjf l;kasjdf ;lkasjf;lkas jfl;as f;las jf;lka sjf;lka sjdfl;k jf;lka sjdf;lk asj;dfl jasj;l 
+                    </p>
+                </div>
+               
+                <div class="Tiana">
+                  
+                    <img
+                      class="img-fluid rounded mb-4 mb-lg-0"
+                      src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                      alt=""
+                      height = "300"
+                    />
+                  <h2>Tiana Aldroubi</h2>
+                  <p class="paragraph">
+                     dkjafhksjdfh lkdjf l;kasjdf ;lkasjf;lkas jfl;as f;las jf;lka sjf;lka sjdfl;k jf;lka sjdf;lk asj;dfl jasj;l 
+                    </p>
+                </div>
+                <div class="Erick">
+                  <img
+                      class="img-fluid rounded mb-4 mb-lg-0"
+                      src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                      alt=""
+                      height = "300"
+                    />
+                  <h2>Erick Tepen</h2>
+                  <p class="paragraph">
+                     dkjafhksjdfh lkdjf l;kasjdf ;lkasjf;lkas jfl;as f;las jf;lka sjf;lka sjdfl;k jf;lka sjdf;lk asj;dfl jasj;l 
+                    </p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+    
+  )
+}
+ 
+export default Aboutus;
