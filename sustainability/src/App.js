7cc284d887f3681d1005dfb5f0331fae7007594b
Ayon Bhowmick <ayb224@lehigh.edu>
2021-11-13 18:15:17 -0500

    popup

--- sustainability/src/App.js
+++ sustainability/src/App.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, componentDidMount } from 'react';
 import MainList from './components/MainList/MainList'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Header/Navbar';
 import Popup from './components/Popup/Popup'
 
-
-
-//const [isOpen, setIsOpen] = useState(false);
-const togglePopup = false;
-
 const App = () => {
-	const [ispopup, togglePopup] = useState(true);
+	const [isPopup, togglePopup] = useState(() => {
+		if (document.cookie === "popup=false") {
+			return false;
+		} else {
+			return true;
+		}
+	});
 
 	const removePopup = async () => {
 		togglePopup(false)
+		document.cookie = "popup=false"
+		console.log(document.cookie);
 	}
 
 	return (
 		<div>
 			<Navbar />
 			<div>
-				{ispopup && <Popup
+				{isPopup && <Popup
 					content={<>
-						<b>Welcome to susfoods!</b>
+						<b>Welcome to SusFoods!</b>
 						<p>We strive to help you eliminate your food waste by keeping track of the expiration dates on your food.
-							Simply click the plus sign in the top right corner of your screen to create a new list and use susfoods as your grocery list. Once you’re finished shopping, add in the expiration date of each item and we’ll keep track of the rest.
+							Simply click the plus sign in the top right corner of your screen to create a new list and use SusFoods as your grocery list. Once you’re finished shopping, add in the expiration date of each item and we’ll keep track of the rest.
 						</p>
 					</>}
 					handleClose={removePopup}
