3fb8684a3a88b02f7bc4446f8ed9bf3f307008f2
Ayon Bhowmick <ayb224@lehigh.edu>
2021-11-13 16:24:24 -0500

    list

--- sustainability/src/components/MainList/MainList.jsx
+++ sustainability/src/components/MainList/MainList.jsx
@@ -1,36 +1,55 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import IconButton from '@mui/material/IconButton';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 const MainList = () => {
-    const list = []; 
-
-    const loadList = async () => {
-        localStorage.setItem("1", "milk, 05/12/2002");
-        localStorage.setItem("2", "cheese, 07/26/2002");
-
+    // const list = []; 
+    const [list, setList] = useState(() => {
+        let arr = [];
         let keys = Object.keys(localStorage);
-            for (let x = 0; x < keys.length; x++) {
+        for (let x = 0; x < keys.length; x++) {
             let data = localStorage.getItem(keys[x]);
             var dataArr = data.split(",");
             let item = {
-            'name' : dataArr[0],
-            'purchaseData' : dataArr[1]
+                'name' : dataArr[0],
+                'purchaseDate' : dataArr[1]
             }
-            list.push(item);
-            console.log(list);
-        } 
-    }
-
-    useEffect(() => {
-        loadList();
-    })
+            arr.push(item);
+        }
+        return arr;
+    });
 
     return(
         <div>
             <IconButton>
                 <AddCircleRoundedIcon />
             </IconButton>
+            <div>
+                {list.map((x) => {
+                    return (
+                        <Box sx={{ flexGrow: 1 }}>
+                            <AppBar position="static">
+                                <Toolbar>
+                                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                        {x.name}
+                                    </Typography>
+                                    <Typography variant="h6" component="div" sx={{flexGrow: 1, color: '#afafff'}}>
+                                        {x.purchaseDate}
+                                    </Typography>
+
+                                </Toolbar>
+                            </AppBar>
+                        </Box>
+                    );
+                })}
+            </div>
         </div>
     );
 }
