c2909686650f83dd5d937c3783ff645c32284b6c
Josh Tripoli <jct324@lehigh.edu>
2021-11-14 02:46:46 -0500

    Added style to add item menu

--- sustainability/src/components/ShoppingList/ShoppingList.js
+++ sustainability/src/components/ShoppingList/ShoppingList.js
@@ -8,11 +8,9 @@ function ShoppingList() {
         <div className="ShoppingList">
             <h1 class="font-weight-light">My Shopping List:</h1>
    
-            <form id = "datePicker">
-                <label for="dt">Date: </label>
-                <input name="dt" type="date" />
-                <input type="submit" />
-            </form>
+
+
+
 
         </div>
     )
