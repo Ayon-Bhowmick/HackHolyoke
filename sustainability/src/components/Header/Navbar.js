753ff8a71a7d88aaf4eac00a51e3ed074fbc3d0c
Josh Tripoli <jct324@lehigh.edu>
2021-11-13 20:05:39 -0500

    Navigation bar links

--- sustainability/src/components/Header/Navbar.js
+++ sustainability/src/components/Header/Navbar.js
@@ -8,12 +8,15 @@ function Navbar() {
       <h1>SusFood</h1>
       <h2> for a sustainable future</h2>
 
-      <div id="titlebox">
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-      </div>
+        <div id="titlebox">
+          <a href="#/">Home</a>
+          <a href="#/AboutUs">About Us</a>
+          <a href="#/Contact">Contact</a>
+        </div>
 
+        <div className="navbar-container">
+          link
+        </div>
     </div>
   )
 }
